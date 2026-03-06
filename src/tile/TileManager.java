3143a12b4414721e947217d5d8c6001c779d806e
PRAJWAL4103 <prajwal0410mp@gmail.com>
2026-03-06 22:29:28 +0530

    mapping tiles

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -1,7 +1,10 @@
 package tile;
 
 import java.awt.Graphics2D;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import javax.imageio.ImageIO;
 
@@ -11,12 +14,13 @@ public class TileManager {
 	
 	GamePanel gp;
 	Tile[] tile;
-	
+	int mapTileNum[][];
 	
 	public TileManager(GamePanel gp) {
 		this.gp = gp;
 		
 		tile = new Tile[10];
+		mapTileNum = int int[gp.maxScreenCol][gp.maxScreenRow]
 		
 		getTileImage();
 	}
@@ -39,9 +43,62 @@ public class TileManager {
 		}
 		
 	}
+	public void loadMap() {
+		
+		try {
+			
+			InputStream is =getClass().getResourceAsStream("/maps/map_01.txt");
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			
+			int col = 0;
+			int row = 0;
+			
+			while(col < gp.maxScreenCol && row < gp.maxScreenRow) {
+				
+				String line = br.readLine();
+				
+				while(col < gp.maxScreenCol) {
+					
+					String numbers[] = line.split(" ");
+					int num = Integer.parseInt(numbers[col]);
+					
+					mapTileNum[col][row] = num;
+					col ++;
+				}
+				if(col == gp.maxScreenCol) {
+					col = 0;
+					row++;
+					
+				}
+				
+			}
+			br.close();
+		}catch(Exception e) {
+			
+		}
+		
+		
+	}
 	public void draw(Graphics2D g2) {
 	
-		g2.drawImage(tile[0].image, 0, 0, gp.tileSize, gp.tileSize, null);
+		int col = 0;
+		int row = 0;
+		int x = 0;
+		int y = 0;
+		
+		while(col < gp.maxScreenCol && row < gp.maxScreenRow) {
+			
+			g2.drawImage(tile[0].image, x, y, gp.tileSize, gp.tileSize, null);
+			col ++;
+			x += gp.tileSize;
+			
+			if(col == gp.maxScreenCol) {
+				col = 0;
+				x = 0;
+				row ++;
+				y += gp.tileSize;
+			}
+		}
 		
 	}
 	
